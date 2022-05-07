7a27131fe27917594c86a87584916732d1cb9e41
Yonatan Mark Liudmirsky <45634836+YonLiud@users.noreply.github.com>
2022-05-07 10:47:06 -0700

    Minimal misc changes to improve developments (#6)
    * Better Fail handling
    
    Change the `fail` block for a more "developer-friendly" environment
    
    * Rename gradlew to gradlew.sh
    
    Renaming the file will enable other developers to easier understand the file tree and not mix it with files like LICENSE and other markdown files

--- gradlew.bat
+++ gradlew.bat
@@ -73,10 +73,10 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
+@rem pause the application before exiting so the user can see the error message and logs
+@rem exit with the errorlevel for further processing
+pause
+exit /b %ERRORLEVEL%
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
