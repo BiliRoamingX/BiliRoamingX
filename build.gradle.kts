89f866d739a7a1868b29ad6695f92e6f91b600f5
oSumAtrIX <johan.melkonyan1@web.de>
2023-01-02 02:48:51 +0100

    refactor: remove workaround task `publish`
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,6 @@ buildscript {
     }
 }
 
-// Tracking issue https://github.com/semantic-release/semantic-release/issues/963
-tasks.register("publish", DefaultTask::class) {
-    group = "publish"
-    description = "Dummy publish to pass the verification phase of the gradle-semantic-release-plugin"
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
