303e65cf240912e8a02e014478ba02ba2adaad08
Kofua <1638183271zjn@gmail.com>
2023-06-09 14:42:38 +0800

    feat(bilibili): block comment word search

---
+++ app/src/main/java/app/revanced/bilibili/patches/CommentReplyUrlPatch.java
@@ -0,0 +1,19 @@
+package app.revanced.bilibili.patches;
+
+import com.bapis.bilibili.main.community.reply.v1.Url;
+import com.google.protobuf.MapFieldLite;
+
+import app.revanced.bilibili.settings.Settings;
+
+public class CommentReplyUrlPatch {
+    public static void filterUrls(MapFieldLite<String, Url> urls) {
+        if (!Settings.BLOCK_WORD_SEARCH.getBoolean()) return;
+        var iterator = urls.entrySet().iterator();
+        while (iterator.hasNext()) {
+            var entry = iterator.next();
+            var url = entry.getValue().getAppUrlSchema();
+            if (url.startsWith("bilibili://search?from=appcommentline_search"))
+                iterator.remove();
+        }
+    }
+}
---
+++ dummy/src/main/java/com/bapis/bilibili/main/community/reply/v1/Url.java
@@ -0,0 +1,7 @@
+package com.bapis.bilibili.main.community.reply.v1;
+
+public class Url {
+    public String getAppUrlSchema() {
+        throw new UnsupportedOperationException("Stub");
+    }
+}
