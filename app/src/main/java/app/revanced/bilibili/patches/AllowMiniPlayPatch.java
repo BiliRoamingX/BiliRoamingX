6ce1b1dd804153f0c262501918d43a4a003f03b2
Kofua <1638183271zjn@gmail.com>
2023-06-26 20:58:46 +0800

    fix(bilibili): typo

--- app/src/main/java/app/revanced/bilibili/patches/AllowMiniPlayPatch.java
+++ app/src/main/java/app/revanced/bilibili/patches/AllowMiniPlayPatch.java
@@ -6,7 +6,7 @@ import app.revanced.bilibili.settings.Settings;
 
 public class AllowMiniPlayPatch {
     public static boolean allowMiniPlay(PlayConfig.PlayConfigType type) {
-        return Settings.UNLOCK_PLAY_LIMIT.getBoolean()
+        return Settings.UNLOCK_AREA_LIMIT.getBoolean()
                 && Settings.ALLOW_MINI_PLAY.getBoolean()
                 && type == PlayConfig.PlayConfigType.MINIPLAYER;
     }
