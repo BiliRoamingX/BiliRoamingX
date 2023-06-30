77e388160ec05c0faca6124057c6e44d1b919558
Kofua <1638183271zjn@gmail.com>
2023-06-30 09:31:14 +0800

    fix(bilibili): replace story video

--- app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
+++ app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
@@ -6,16 +6,20 @@ import app.revanced.bilibili.settings.Settings;
 import app.revanced.bilibili.utils.LogHelper;
 
 public class BLRoutePatch {
+    private static final String STORY_ROUTER_PARAM = "&-Arouter=story";
+
     public static Uri intercept(Uri uri) {
         String scheme = uri.getScheme();
+        String url;
         if ("bilibili".equals(scheme)) {
-            if (Settings.REPLACE_STORY_VIDEO.getBoolean()
-                    && "story".equals(uri.getAuthority())) {
-                return uri.buildUpon().authority("video")
-                        .build();
+            if (Settings.REPLACE_STORY_VIDEO.getBoolean()) {
+                if ("story".equals(uri.getAuthority()))
+                    return uri.buildUpon().authority("video").build();
+                else if ("video".equals(uri.getAuthority()) && (url = uri.toString()).contains(STORY_ROUTER_PARAM))
+                    return Uri.parse(url.replace(STORY_ROUTER_PARAM, ""));
             }
         }
-        LogHelper.debug(() -> "kofua, route uri: " + uri);
+        LogHelper.debug(() -> "Route uri: " + uri);
         return uri;
     }
 }
