d111fc51e0fa4ac7390dba6de27c0286a9e20c3b
Kofua <1638183271zjn@gmail.com>
2023-07-02 11:38:29 +0800

    feat(bilibili): enhance blocking up recommend ads

--- app/src/main/java/app/revanced/bilibili/patches/okhttp/OkHttpPatch.java
+++ app/src/main/java/app/revanced/bilibili/patches/okhttp/OkHttpPatch.java
@@ -7,10 +7,12 @@ import android.text.TextUtils;
 import androidx.annotation.NonNull;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import app.revanced.bilibili.api.BiliRoamingApi;
 import app.revanced.bilibili.settings.Settings;
+import app.revanced.bilibili.utils.Jsons;
 import app.revanced.bilibili.utils.LogHelper;
 import app.revanced.bilibili.utils.SubtitleHelper;
 import app.revanced.bilibili.utils.Utils;
@@ -19,20 +21,17 @@ import app.revanced.bilibili.utils.Utils;
 public class OkHttpPatch {
     private static final String RES_NO_UPDATE = "{\"code\":-304,\"message\":\"没有改动\"}";
     private static final String RES_MATERIAL_EMPTY = "{\"code\":0,\"data\":{\"container\":[]},\"message\":\"success\"}";
+    private static final String RES_CARDS_EMPTY = "{\"code\":0,\"data\":[]}";
 
     public static boolean shouldHook(String url, int code) {
-        if (shouldConvertSubtitle(url, code)) {
-            return true;
-        } else if (shouldUnlockBangumi(url, code)) {
-            return true;
-        } else if (shouldFixSpace(url, code)) {
-            return true;
-        } else if (shouldBlockUpdate(url, code)) {
-            return true;
-        } else if (shouldBlockBangumiPageAds(url, code)) {
-            return true;
-        }
-        return false;
+        return shouldConvertSubtitle(url, code)
+                || shouldUnlockBangumi(url, code)
+                || shouldFixSpace(url, code)
+                || shouldBlockUpdate(url, code)
+                || shouldBlockBangumiMaterial(url, code)
+                || shouldPurifySeasonRcmd(url, code)
+                || shouldPurifyCards(url, code)
+                /* for convenient to add a new line */;
     }
 
     @SuppressLint("DefaultLocale")
@@ -48,8 +47,12 @@ public class OkHttpPatch {
             return fixSpace(url, response);
         } else if (shouldBlockUpdate(url, code)) {
             return RES_NO_UPDATE;
-        } else if (shouldBlockBangumiPageAds(url, code)) {
+        } else if (shouldBlockBangumiMaterial(url, code)) {
             return RES_MATERIAL_EMPTY;
+        } else if (shouldPurifySeasonRcmd(url, code)) {
+            return purifySeasonRcmd(response);
+        } else if (shouldPurifyCards(url, code)) {
+            return RES_CARDS_EMPTY;
         }
         return response;
     }
@@ -76,10 +79,23 @@ public class OkHttpPatch {
                 && code == HttpURLConnection.HTTP_OK;
     }
 
-    private static boolean shouldBlockBangumiPageAds(String url, int code) {
+    private static boolean shouldBlockBangumiMaterial(String url, int code) {
         return Settings.BLOCK_BANGUMI_PAGE_ADS.getBoolean()
                 && (url.startsWith("https://api.bilibili.com/pgc/activity/deliver/material/receive")
                 || url.startsWith("https://api.bilibili.com/pgc/activity/deliver/material/receive-activity"))
+                && code == HttpURLConnection.HTTP_OK;
+    }
+
+    public static boolean shouldPurifySeasonRcmd(String url, int code) {
+        return Settings.REMOVE_RELATE_PROMOTE.getBoolean()
+                && url.startsWith("https://api.bilibili.com/pgc/season/app/related/recommend")
+                && code == HttpURLConnection.HTTP_OK;
+    }
+
+    public static boolean shouldPurifyCards(String url, int code) {
+        return Settings.BLOCK_UP_RCMD_ADS.getBoolean()
+                && (url.startsWith("https://api.bilibili.com/pgc/season/player/cards")
+                || url.startsWith("https://api.bilibili.com/pgc/season/player/ogv/cards"))
                 && code == HttpURLConnection.HTTP_OK;
     }
 
@@ -128,5 +144,24 @@ public class OkHttpPatch {
         if (data == null)
             return response;
         return "{\"code\":0,\"data\":" + data + "}";
+    }
+
+    static String purifySeasonRcmd(String response) {
+        var json = Jsons.toJSONObject(response);
+        var result = json.optJSONObject("result");
+        if (result == null) return response;
+        var cards = result.optJSONArray("cards");
+        if (cards == null || cards.length() == 0) return response;
+        var toRemoveIdxList = new ArrayList<Integer>();
+        for (int i = 0; i < cards.length(); i++) {
+            var card = cards.optJSONObject(i);
+            if (card.optInt("type") == 2)
+                toRemoveIdxList.add(i);
+        }
+        if (toRemoveIdxList.isEmpty())
+            return response;
+        for (int i = toRemoveIdxList.size() - 1; i >= 0; i--)
+            cards.remove(toRemoveIdxList.get(i));
+        return json.toString();
     }
 }
