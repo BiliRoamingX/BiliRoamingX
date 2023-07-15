f06962b07245914a0115b297e23db5eeb9bbd179
Kofua <1638183271zjn@gmail.com>
2023-07-15 22:19:17 +0800

    fix(bilibili): customize video quality for bangumi

--- app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
+++ app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
@@ -2,11 +2,14 @@ package app.revanced.bilibili.patches;
 
 import android.net.Uri;
 
+import java.util.regex.Pattern;
+
 import app.revanced.bilibili.settings.Settings;
 import app.revanced.bilibili.utils.LogHelper;
 
 public class BLRoutePatch {
     private static final String STORY_ROUTER_PARAM = "&-Arouter=story";
+    private static final Pattern playerPreloadRegex = Pattern.compile("&player_preload=[^&]*");
 
     public static Uri intercept(Uri uri) {
         String scheme = uri.getScheme();
@@ -18,6 +21,10 @@ public class BLRoutePatch {
                 else if ("video".equals(uri.getAuthority()) && (url = uri.toString()).contains(STORY_ROUTER_PARAM))
                     return Uri.parse(url.replace(STORY_ROUTER_PARAM, ""));
             }
+        } else if ("https".equals(scheme)) {
+            boolean needHook = VideoQualityPatch.halfScreenQuality() != 0 || VideoQualityPatch.fullScreenQuality() != 0;
+            if (needHook && (url = uri.toString()).startsWith("https://www.bilibili.com/bangumi/play"))
+                return Uri.parse(playerPreloadRegex.matcher(url).replaceAll(""));
         }
         LogHelper.debug(() -> "Route uri: " + uri);
         return uri;
