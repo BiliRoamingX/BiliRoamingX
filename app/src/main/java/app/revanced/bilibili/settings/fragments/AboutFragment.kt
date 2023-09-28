1f023e6f4650ed16f5f840cabb9a00928af5496f
Kofua <1638183271zjn@gmail.com>
2023-09-28 19:17:48 +0800

    chore(bilibili): move debug preference to about page

--- app/src/main/java/app/revanced/bilibili/settings/fragments/AboutFragment.kt
+++ app/src/main/java/app/revanced/bilibili/settings/fragments/AboutFragment.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import androidx.preference.Preference
 import app.revanced.bilibili.utils.Constants
 import app.revanced.bilibili.utils.Utils
+import app.revanced.bilibili.utils.logFile
 import app.revanced.bilibili.utils.onClick
 
 class AboutFragment : BiliRoamingBaseSettingFragment("biliroaming_setting_about") {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findPreference<Preference>("version")?.summary = Constants.VERSION
+        findPreference<Preference>("debug")?.summary =
+            Utils.getString("biliroaming_debug_summary", logFile.absolutePath)
         findPreference<Preference>("feedback")?.onClick {
             val uri = Uri.parse(Utils.getString("biliroaming_feedback_url"))
             val intent = Intent(Intent.ACTION_VIEW, uri)
--- app/src/main/java/app/revanced/bilibili/settings/fragments/BiliRoamingSettingsFragment.kt
+++ app/src/main/java/app/revanced/bilibili/settings/fragments/BiliRoamingSettingsFragment.kt
@@ -3,14 +3,11 @@ package app.revanced.bilibili.settings.fragments
 import android.os.Bundle
 import androidx.preference.Preference
 import app.revanced.bilibili.utils.Utils
-import app.revanced.bilibili.utils.logFile
 import app.revanced.bilibili.utils.onClick
 
 class BiliRoamingSettingsFragment : BiliRoamingBaseSettingFragment("biliroaming_settings") {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         super.onCreatePreferences(savedInstanceState, rootKey)
-        findPreference<Preference>("debug")?.summary =
-            Utils.getString("biliroaming_debug_summary", logFile.absolutePath)
         findPreference<Preference>("reboot")?.onClick { Utils.reboot(); true }
     }
 }
