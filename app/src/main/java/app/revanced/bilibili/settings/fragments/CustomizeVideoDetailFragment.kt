56f9aa2e68990467a52425f89f76fbeb3eed33a0
Kofua <1638183271zjn@gmail.com>
2024-01-02 09:36:00 +0800

    feat(bilibili): disable "disable auto select" preference for 7.63.0+

--- app/src/main/java/app/revanced/bilibili/settings/fragments/CustomizeVideoDetailFragment.kt
+++ app/src/main/java/app/revanced/bilibili/settings/fragments/CustomizeVideoDetailFragment.kt
@@ -3,6 +3,7 @@ package app.revanced.bilibili.settings.fragments
 import android.os.Bundle
 import app.revanced.bilibili.settings.Settings
 import app.revanced.bilibili.utils.Utils
+import app.revanced.bilibili.utils.Versions
 
 class CustomizeVideoDetailFragment :
     BiliRoamingBaseSettingFragment("biliroaming_setting_customize_video_detail") {
@@ -10,6 +11,9 @@ class CustomizeVideoDetailFragment :
         super.onCreatePreferences(savedInstanceState, rootKey)
         disablePreference(Settings.BLOCK_FAN_GUIDE.key, PrefsDisableReason.NEW_PLAYER) {
             !Utils.newPlayerEnabled
+        }
+        disablePreference(Settings.DISABLE_AUTO_SELECT.key, PrefsDisableReason.APP_VERSION) {
+            Versions.ge7_63_0()
         }
     }
 }
