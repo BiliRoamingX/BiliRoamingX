77ecbee0f8af26d8e420fa7e76d3ecb5cd58fca4
Kofua <1638183271zjn@gmail.com>
2024-02-14 20:28:12 +0800

    build(bilibili): bump to 1.18

--- app/src/main/java/app/revanced/bilibili/utils/Constants.java
+++ app/src/main/java/app/revanced/bilibili/utils/Constants.java
@@ -1,7 +1,7 @@
 package app.revanced.bilibili.utils;
 
 public class Constants {
-    public static final String VERSION = "1.17";
+    public static final String VERSION = "1.18";
     public static final String PINK_PACKAGE_NAME = "tv.danmaku.bili";
     public static final String BLUE_PACKAGE_NAME = "com.bilibili.app.blue";
     public static final String PLAY_PACKAGE_NAME = "com.bilibili.app.in";
