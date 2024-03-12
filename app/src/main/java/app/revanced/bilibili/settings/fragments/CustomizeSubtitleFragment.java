52ab845a5599bdb9a8e8076103c964fcb93965f5
Kofua <1638183271zjn@gmail.com>
2024-03-13 01:00:13 +0800

    feat(bilibili): move subtitle preferences to player category

--- app/src/main/java/app/revanced/bilibili/settings/fragments/CustomizeSubtitleFragment.java
+++
@@ -1,7 +0,0 @@
-package app.revanced.bilibili.settings.fragments;
-
-public class CustomizeSubtitleFragment extends BiliRoamingBaseSettingFragment {
-    public CustomizeSubtitleFragment() {
-        super("biliroaming_setting_customize_subtitle");
-    }
-}
