18367c7a9e41591b78f6243e47fa8a9695acd4e8
Kofua <1638183271zjn@gmail.com>
2024-03-13 00:51:15 +0800

    fix(bilibili): switch dark dialog for hd

--- app/src/main/java/app/revanced/bilibili/patches/DarkSwitchPatch.java
+++ app/src/main/java/app/revanced/bilibili/patches/DarkSwitchPatch.java
@@ -8,6 +8,7 @@ import app.revanced.bilibili.settings.Settings;
 import app.revanced.bilibili.utils.Utils;
 import tv.danmaku.bili.ui.main2.mine.HomeUserCenterFragment;
 import tv.danmaku.bilibilihd.ui.main.mine.HdHomeUserCenterFragment;
+import tv.danmaku.bilibilihd.ui.main.mine.common.HdCommonBaseFragment;
 
 @Keep
 public class DarkSwitchPatch {
@@ -28,6 +29,22 @@ public class DarkSwitchPatch {
     }
 
     public static void switchDarkMode(HdHomeUserCenterFragment fragment, boolean report) {
+        if (!Settings.SWITCH_DARK_DIALOG.getBoolean()) {
+            fragment.switchDarkMode_Origin(report);
+            return;
+        }
+        if (Utils.isNightFollowSystem()) {
+            new AlertDialog.Builder(fragment.getContext())
+                    .setMessage(Utils.getString("biliroaming_switch_dark_tips"))
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> fragment.switchDarkMode_Origin(report))
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .show();
+        } else {
+            fragment.switchDarkMode_Origin(report);
+        }
+    }
+
+    public static void switchDarkMode(HdCommonBaseFragment fragment, boolean report) {
         if (!Settings.SWITCH_DARK_DIALOG.getBoolean()) {
             fragment.switchDarkMode_Origin(report);
             return;
---
+++ dummy/src/main/java/tv/danmaku/bilibilihd/ui/main/mine/common/HdCommonBaseFragment.java
@@ -0,0 +1,9 @@
+package tv.danmaku.bilibilihd.ui.main.mine.common;
+
+import androidx.fragment.app.Fragment;
+
+public class HdCommonBaseFragment extends Fragment {
+    // method will added by patcher
+    public void switchDarkMode_Origin(boolean report) {
+    }
+}
