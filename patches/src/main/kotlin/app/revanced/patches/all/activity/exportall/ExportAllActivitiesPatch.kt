6f443316cbb1522c752a3dcdef2135c02e83fc9e
Kofua <1638183271zjn@gmail.com>
2024-04-18 23:10:58 +0800

    refactor: export all activity patch

--- patches/src/main/kotlin/app/revanced/patches/all/activity/exportall/ExportAllActivitiesPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/activity/exportall/ExportAllActivitiesPatch.kt
@@ -3,6 +3,9 @@ package app.revanced.patches.all.activity.exportall
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.util.children
+import app.revanced.util.get
+import app.revanced.util.set
 
 @Patch(
     name = "Export all activities",
@@ -11,26 +14,11 @@ import app.revanced.patcher.patch.annotation.Patch
 )
 @Suppress("unused")
 object ExportAllActivitiesPatch : ResourcePatch() {
-    private const val EXPORTED_FLAG = "android:exported"
     override fun execute(context: ResourceContext) {
-        context.xmlEditor["AndroidManifest.xml"].use { editor ->
-            val document = editor.file
-            val activities = document.getElementsByTagName("activity")
-
-            for (i in 0..activities.length) {
-                activities.item(i)?.apply {
-                    val exportedAttribute = attributes.getNamedItem(EXPORTED_FLAG)
-
-                    if (exportedAttribute != null) {
-                        if (exportedAttribute.nodeValue != "true")
-                            exportedAttribute.nodeValue = "true"
-                    }
-                    // Reason why the attribute is added in the case it does not exist:
-                    // https://github.com/revanced/revanced-patches/pull/1751/files#r1141481604
-                    else document.createAttribute(EXPORTED_FLAG)
-                        .apply { value = "true" }
-                        .let(attributes::setNamedItem)
-                }
+        context.document["AndroidManifest.xml"].use { dom ->
+            dom["application"].children().forEach { child ->
+                if (child.tagName == "activity")
+                    child["android:exported"] = "true"
             }
         }
     }
