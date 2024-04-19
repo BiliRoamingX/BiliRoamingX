888b113ae4bf7b7031383b107d102e127dbd0c7b
Kofua <1638183271zjn@gmail.com>
2024-04-19 17:52:40 +0800

    chore: refine patches build gradle script

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.gradle.kotlin.dsl.support.listFilesOrdered
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -9,7 +8,6 @@ plugins {
 }
 
 group = "app.revanced.bilibili"
-archivesName = "${rootProject.name}-${project.name}"
 
 dependencies {
     implementation(libs.revanced.patcher)
@@ -28,61 +26,58 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
-tasks {
-    withType(Jar::class) {
-        exclude("app/revanced/generator")
+tasks.jar {
+    archiveBaseName = "${rootProject.name}-${project.name}"
+    exclude("app/revanced/generator")
 
-        manifest {
-            attributes["Name"] = "BiliRoamingX Patches"
-            attributes["Description"] = "Patches for BiliRoamingX."
-            attributes["Version"] = version
-            attributes["Timestamp"] = System.currentTimeMillis().toString()
-            attributes["Source"] = "git@github.com:BiliRoamingX/BiliRoamingX.git"
-            attributes["Author"] = "Kofua"
-            attributes["License"] = "GNU General Public License v3.0"
+    manifest {
+        attributes["Name"] = "BiliRoamingX Patches"
+        attributes["Description"] = "Patches for BiliRoamingX."
+        attributes["Version"] = version
+        attributes["Timestamp"] = System.currentTimeMillis().toString()
+        attributes["Source"] = "git@github.com:BiliRoamingX/BiliRoamingX.git"
+        attributes["Author"] = "Kofua"
+        attributes["License"] = "GNU General Public License v3.0"
+    }
+}
+
+tasks.register("buildDexJar") {
+    description = "Build and add a DEX to the JAR file"
+    group = "build"
+
+    dependsOn(tasks.build)
+
+    doLast {
+        val d8 = File(System.getenv("ANDROID_HOME")).resolve("build-tools")
+            .listFilesOrdered().last().resolve("d8").absolutePath
+
+        val patchesJar = configurations.archives.get().allArtifacts.files.files.first().absolutePath
+        val workingDirectory = layout.buildDirectory.dir("libs").get().asFile
+
+        exec {
+            workingDir = workingDirectory
+            commandLine = listOf(d8, "--release", patchesJar)
+        }
+
+        exec {
+            workingDir = workingDirectory
+            commandLine = listOf("zip", "-u", patchesJar, "classes.dex")
         }
     }
+}
 
-    register("buildDexJar") {
-        description = "Build and add a DEX to the JAR file"
-        group = "build"
+tasks.register<JavaExec>("generatePatchesFiles") {
+    description = "Generate patches files"
 
-        dependsOn(build)
+    dependsOn(tasks.build)
 
-        doLast {
-            val d8 = File(System.getenv("ANDROID_HOME")).resolve("build-tools")
-                .listFilesOrdered().last().resolve("d8").absolutePath
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("app.revanced.generator.Main")
+}
 
-            val patchesJar = configurations.archives.get().allArtifacts.files.files.first().absolutePath
-            val workingDirectory = layout.buildDirectory.dir("libs").get().asFile
-
-            exec {
-                workingDir = workingDirectory
-                commandLine = listOf(d8, "--release", patchesJar)
-            }
-
-            exec {
-                workingDir = workingDirectory
-                commandLine = listOf("zip", "-u", patchesJar, "classes.dex")
-            }
-        }
-    }
-
-    register<JavaExec>("generatePatchesFiles") {
-        description = "Generate patches files"
-
-        dependsOn(build)
-
-        classpath = sourceSets["main"].runtimeClasspath
-        mainClass.set("app.revanced.generator.Main")
-    }
-
-    // Needed by gradle-semantic-release-plugin.
-    // Tracking: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
-    publish {
-        dependsOn("buildDexJar")
-        dependsOn("generatePatchesFiles")
-    }
+tasks.publish {
+    dependsOn("buildDexJar")
+    dependsOn("generatePatchesFiles")
 }
 
 tasks.register("dist") {
@@ -97,8 +92,10 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/BiliRoamingX/BiliRoamingX")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
