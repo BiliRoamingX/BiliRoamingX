51451825617445447c718d962cc69d61c3b19c4a
Kofua <1638183271zjn@gmail.com>
2024-04-24 12:12:28 +0800

    fix: live room display

--- integrations/app/src/main/java/app/revanced/bilibili/patches/DpiPatch.java
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/DpiPatch.java
@@ -1,7 +1,6 @@
 package app.revanced.bilibili.patches;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 
 import androidx.annotation.Keep;
@@ -21,10 +20,9 @@ public class DpiPatch {
         if (scale == 0f)
             return density;
         Activity topActivity = ApplicationDelegate.getTopActivity();
-        if (Versions.ge7_70_0() && topActivity instanceof LiveRoomActivityV3
-                && topActivity.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+        if (Versions.ge7_70_0() && topActivity instanceof LiveRoomActivityV3) {
             var sysDensity = Resources.getSystem().getDisplayMetrics().density;
-            // for pink client 7.70.0+, live room player need right density to show content when portrait orientation.
+            // for pink client 7.70.0+, live room player need right density to show content.
             return sysDensity + scale;
         } else {
             // return system default density for making subtitle and danmaku size display normally.
