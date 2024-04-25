12b353ba14a8d770d884c6c72e6728b6a678330d
Kofua <1638183271zjn@gmail.com>
2024-04-25 20:24:21 +0800

    fix: dynamic video open crash when forcing old player
    Fix: #158

--- integrations/app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.java
@@ -23,6 +23,7 @@ public class BLRoutePatch {
         String scheme = uri.getScheme();
         String url = uri.toString();
         if ("bilibili".equals(scheme)) {
+            boolean needRemovePayload = VideoQualityPatch.halfScreenQuality() != 0 || VideoQualityPatch.getMatchedFullScreenQuality() != 0 || Settings.DEFAULT_PLAYBACK_SPEED.getFloat() != 0f;
             String authority = uri.getEncodedAuthority();
             if ("story".equals(authority) || "video".equals(authority)) {
                 Uri.Builder newUri = uri.buildUpon();
@@ -32,14 +33,25 @@ public class BLRoutePatch {
                 if (!TextUtils.isEmpty(newQuery)) {
                     if (Settings.REPLACE_STORY_VIDEO.getBoolean())
                         newQuery = newQuery.replace(STORY_ROUTER_QUERY, "").replace(STORY_TYPE_QUERY, "");
-                    boolean needRemovePayload = VideoQualityPatch.halfScreenQuality() != 0 || VideoQualityPatch.getMatchedFullScreenQuality() != 0 || Settings.DEFAULT_PLAYBACK_SPEED.getFloat() != 0f;
                     if (needRemovePayload)
                         newQuery = playerPreloadRegex.matcher(newQuery).replaceAll("");
                 }
                 newUri.encodedQuery(newQuery);
                 return newUri.build();
-            } else if ("1".equals(Settings.PLAYER_VERSION.getString()) && url.startsWith("bilibili://music/playlist/playpage") && !url.equals("bilibili://music/playlist/playpage/0")) {
-                return uri.buildUpon().appendQueryParameter("force_old_playlist", "1").build();
+            } else if (url.startsWith("bilibili://music/playlist/playpage") && !url.equals("bilibili://music/playlist/playpage/0")) {
+                String aid = uri.getQueryParameter("aid");
+                String pageType = uri.getQueryParameter("page_type");
+                var newUri = uri.buildUpon();
+                if (needRemovePayload) {
+                    String newQuery = playerPreloadRegex.matcher(uri.getEncodedQuery()).replaceAll("");
+                    newUri.encodedQuery(newQuery);
+                }
+                if ("1".equals(Settings.PLAYER_VERSION.getString())) {
+                    newUri.appendQueryParameter("force_old_playlist", "1");
+                    if ("10".equals(pageType) && !TextUtils.isEmpty(aid))
+                        newUri.encodedAuthority("video").encodedPath(aid);
+                }
+                return newUri.build();
             } else if (Settings.ADD_CHANNEL.getBoolean() && url.startsWith("bilibili://pegasus/channel/v2")) {
                 // for hd, consistent with the default behavior of selecting "select" tab before deleting the "topic" tab
                 if (TextUtils.isEmpty(uri.getQueryParameter("tab")))
