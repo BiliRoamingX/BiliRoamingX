0b0b07f5c672220116ac640d849f8a18b7db0f47
Kofua <1638183271zjn@gmail.com>
2024-04-27 23:32:14 +0800

    fix: card click processor fingerprint

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/json/fingerprints/CardClickProcessorFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/json/fingerprints/CardClickProcessorFingerprint.kt
@@ -3,5 +3,11 @@ package app.revanced.patches.bilibili.misc.json.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object CardClickProcessorFingerprint : MethodFingerprint(
-    strings = listOf("action:feed:dislike_reason", "action:feed:dislike_toast")
+    strings = listOf("action:feed:dislike_reason", "action:feed:dislike_toast"),
+    returnType = "V",
+    customFingerprint = { methodDef, _ ->
+        methodDef.parameterTypes.count {
+            it == "Lcom/bilibili/app/comm/list/common/data/DislikeReason;"
+        } == 2
+    }
 )
