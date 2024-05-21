97fe0206767b5d26db401d076726fbf3b0df2fb1
Kofua <1638183271zjn@gmail.com>
2024-05-21 13:55:24 +0800

    fix: show watching countdown config
    Close #265

--- integrations/app/src/main/java/app/revanced/bilibili/patches/okhttp/hooks/BangumiMaterial.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/okhttp/hooks/BangumiMaterial.kt
@@ -2,6 +2,9 @@ package app.revanced.bilibili.patches.okhttp.hooks
 
 import app.revanced.bilibili.patches.okhttp.ApiHook
 import app.revanced.bilibili.settings.Settings
+import app.revanced.bilibili.utils.toJSONObject
+import org.json.JSONArray
+import org.json.JSONObject
 
 object BangumiMaterial : ApiHook() {
     override fun shouldHook(url: String, status: Int): Boolean {
@@ -12,6 +15,21 @@ object BangumiMaterial : ApiHook() {
     }
 
     override fun hook(url: String, status: Int, request: String, response: String): String {
-        return """{"code":0,"data":{"container":[]},"message":"success"}"""
+        val json = response.toJSONObject()
+        if (json.optInt("code", -1) == 0) {
+            val newData = JSONObject().apply {
+                put("container", JSONArray())
+            }
+            val newResp = JSONObject().apply {
+                put("code", 0)
+                put("message", "success")
+                put("data", newData)
+            }
+            json.optJSONObject("data")?.optJSONObject("watch_count_down_cfg")?.let {
+                newData.put("watch_count_down_cfg", it)
+            }
+            return newResp.toString()
+        }
+        return response
     }
 }
