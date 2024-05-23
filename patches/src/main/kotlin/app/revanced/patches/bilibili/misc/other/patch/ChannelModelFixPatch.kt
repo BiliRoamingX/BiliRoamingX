890cea5ffc642289a474e2e91f7a88b7f0b3e1b6
Kofua <1638183271zjn@gmail.com>
2024-05-23 10:26:53 +0800

    fix: channel model classes default constructor

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/ChannelModelFixPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/ChannelModelFixPatch.kt
@@ -6,9 +6,11 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableClass
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 import app.revanced.patches.bilibili.utils.*
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.iface.ClassDef
 import com.android.tools.smali.dexlib2.iface.value.StringEncodedValue
 
 @Patch(
@@ -22,34 +24,44 @@ import com.android.tools.smali.dexlib2.iface.value.StringEncodedValue
 )
 object ChannelModelFixPatch : BytecodePatch() {
     override fun execute(context: BytecodeContext) {
-        val fieldsNames = setOf("stick", "normal", "scaned", "config")
-        val modelClassDef = context.classes.find { c ->
-            c.fields.mapNotNull { f ->
-                f.annotation("Lcom/alibaba/fastjson/annotation/JSONField;")
-                    ?.value<StringEncodedValue>("name")?.value
-            }.toSet() == fieldsNames
-        } ?: throw PatchException("not found channel model class")
-        modelClassDef.memberClasses().map { t ->
-            context.classes.first { it.type == t }
-        }.forEach { c ->
-            context.proxy(c).mutableClass.run {
-                if (methods.none { it.name == "<init>" && it.parameterTypes.isEmpty() }) {
-                    Method(
-                        definingClass = type,
-                        name = "<init>",
-                        returnType = "V",
-                        accessFlags = AccessFlags.PUBLIC.value or AccessFlags.CONSTRUCTOR.value,
-                        implementation = MethodImplementation(registerCount = 1),
-                    ).toMutable().apply {
-                        addInstructions(
-                            0, """
+        fun BytecodeContext.findJSONFieldAnnotationMatchedClass(fieldsNames: Set<String>): ClassDef? {
+            return classes.find { c ->
+                c.fields.mapNotNull { f ->
+                    f.annotation("Lcom/alibaba/fastjson/annotation/JSONField;")
+                        ?.value<StringEncodedValue>("name")?.value
+                }.toSet() == fieldsNames
+            }
+        }
+
+        fun MutableClass.addDefaultConstructorIfNeeded() {
+            if (methods.none { it.name == "<init>" && it.parameterTypes.isEmpty() }) {
+                Method(
+                    definingClass = type,
+                    name = "<init>",
+                    returnType = "V",
+                    accessFlags = AccessFlags.PUBLIC.value or AccessFlags.CONSTRUCTOR.value,
+                    implementation = MethodImplementation(registerCount = 1),
+                ).toMutable().apply {
+                    addInstructions(
+                        0, """
                             invoke-direct {p0}, Ljava/lang/Object;-><init>()V
                             return-void
                         """.trimIndent()
-                        )
-                    }.also { methods.add(it) }
-                }
+                    )
+                }.also { methods.add(it) }
             }
         }
+
+        val channelClassDef = context.findJSONFieldAnnotationMatchedClass(setOf("stick", "normal", "scaned", "config"))
+            ?: throw PatchException("not found channel model class")
+        channelClassDef.memberClasses().map { t ->
+            context.classes.first { it.type == t }
+        }.forEach { c ->
+            context.proxy(c).mutableClass.addDefaultConstructorIfNeeded()
+        }
+
+        val extendClassDef = context.findJSONFieldAnnotationMatchedClass(setOf("label", "model_type", "items"))
+            ?: throw PatchException("not found extend model class")
+        context.proxy(extendClassDef).mutableClass.addDefaultConstructorIfNeeded()
     }
 }
