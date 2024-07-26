83e8faa575f893c059d579433ad306c840607801
Kofua <1638183271zjn@gmail.com>
2024-07-27 02:33:26 +0800

    feat: support open thailand bangumi url directly

--- integrations/app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/BLRoutePatch.kt
@@ -13,6 +13,8 @@ object BLRoutePatch {
     private const val STORY_ROUTER_QUERY = "&-Arouter=story"
     private const val STORY_TYPE_QUERY = "&-Atype=story"
     private val playerPreloadRegex = Regex("&player_preload=[^&]*")
+    private val globalBgmUrlRegex =
+        Regex("""^https?://www\.(?:bilibili\.tv|biliintl\.com)/\w+/play/(?<sid>\d+)(?:/(?<epId>\d+)?)?.*""")
 
     private fun needRemovePayload(): Boolean {
         return VideoQualityPatch.halfScreenQuality() != 0 || VideoQualityPatch.getMatchedFullScreenQuality() != 0 || Settings.DefaultPlaybackSpeed() != 0f
@@ -23,9 +25,10 @@ object BLRoutePatch {
     fun intercept(uri: Uri?): Uri? {
         if (uri == null) return null
         Logger.debug { "Route uri: $uri" }
-        val scheme = uri.scheme
+        val scheme = uri.scheme.orEmpty()
         val url = uri.toString()
         if (Versions.ge8_5_0() && (url.startsWith("https://www.bilibili.com/read")
+                    || url.startsWith("http://www.bilibili.com/read")
                     || (url.startsWith("bilibili://article") && uri.pathSegments.size == 1))
         ) {
             val id = uri.lastPathSegment.orEmpty().removePrefix("cv")
@@ -82,13 +85,13 @@ object BLRoutePatch {
                 if (uri.getQueryParameter("tab").isNullOrEmpty())
                     return uri.buildUpon().appendQueryParameter("tab", "select").build()
             }
-        } else if (scheme == "https") {
-            if (url.startsWith("https://www.bilibili.com/bangumi/play")) {
+        } else if (scheme.startsWith("http")) {
+            if (url.contains("www.bilibili.com/bangumi/play")) {
                 if (needRemovePayload())
                     return Uri.parse(url.replace(playerPreloadRegex, ""))
-            } else if (Settings.ReplaceStoryVideo() && url.startsWith("https://www.bilibili.com/video")) {
+            } else if (Settings.ReplaceStoryVideo() && url.contains("www.bilibili.com/video")) {
                 return Uri.parse(url.replace(STORY_ROUTER_QUERY, "").replace(STORY_TYPE_QUERY, ""))
-            } else if (Settings.DefaultMaxQn() && url.startsWith("https://live.bilibili.com")) {
+            } else if (Settings.DefaultMaxQn() && url.contains("live.bilibili.com")) {
                 val lastPathSegment = uri.pathSegments.singleOrNull()
                 if (!lastPathSegment.isNullOrEmpty() && lastPathSegment.all { it.isDigit() }) {
                     val builder = uri.buildUpon().clearQuery()
@@ -99,6 +102,16 @@ object BLRoutePatch {
                         }
                     }
                     return builder.build()
+                }
+            } else if (url.contains("www.bilibili.tv") || url.contains("www.biliintl.com")) {
+                globalBgmUrlRegex.matchEntire(url)?.run {
+                    val sid = groups["sid"]?.value.orEmpty()
+                    val epId = groups["epId"]?.value.orEmpty()
+                    if (epId.isEmpty()) {
+                        return Uri.parse("https://www.bilibili.com/bangumi/play/ss$sid")
+                    } else {
+                        return Uri.parse("https://www.bilibili.com/bangumi/play/ep$epId?season_id=$sid")
+                    }
                 }
             }
         }
--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/InjectMoreSchemePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/InjectMoreSchemePatch.kt
@@ -69,6 +69,26 @@ object InjectMoreSchemePatch : ResourcePatch() {
                     this["android:scheme"] = "https"
                     this["android:host"] = "t.bilibili.com"
                 }
+                appendChild("data") {
+                    this["android:scheme"] = "http"
+                    this["android:host"] = "www.bilibili.tv"
+                    this["android:pathPattern"] = "/.*/play/.*"
+                }
+                appendChild("data") {
+                    this["android:scheme"] = "https"
+                    this["android:host"] = "www.bilibili.tv"
+                    this["android:pathPattern"] = "/.*/play/.*"
+                }
+                appendChild("data") {
+                    this["android:scheme"] = "http"
+                    this["android:host"] = "www.biliintl.com"
+                    this["android:pathPattern"] = "/.*/play/.*"
+                }
+                appendChild("data") {
+                    this["android:scheme"] = "https"
+                    this["android:host"] = "www.biliintl.com"
+                    this["android:pathPattern"] = "/.*/play/.*"
+                }
             }
         }
     }
