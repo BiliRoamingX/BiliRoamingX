e17d0de7331a25d36640779341a7d319af3c26ff
Kofua <1638183271zjn@gmail.com>
2024-07-30 19:15:10 +0800

    chore: no need manual align page
    Dobby will handle it.

--- integrations/app/src/main/jni/biliroamingx.cc
+++ integrations/app/src/main/jni/biliroamingx.cc
@@ -1,8 +1,6 @@
 #include <jni.h>
 #include <cstdlib>
 #include <android/log.h>
-#include <unistd.h>
-#include <sys/mman.h>
 #include "dobby.h"
 
 #define LOG_TAG "BiliRoamingX"
@@ -16,9 +14,7 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-#define PAGE_ALIGN(address, page_size) ((address) & (-(page_size)))
-
-void fake_exit(int32_t status) {
+void fake_exit(int status) {
     LOGI("Exit function fake success, status: %d", status);
 }
 
@@ -27,9 +23,6 @@ jint JNI_OnLoad(JavaVM *vm, void *) {
     JNIEnv *env;
     if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK)
         return JNI_ERR;
-    const auto page_size = getpagesize();
-    const auto aligned = PAGE_ALIGN((uintptr_t) exit, page_size);
-    mprotect((void *) aligned, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
     if (DobbyHook((void *) exit,
                   reinterpret_cast<dobby_dummy_func_t>(fake_exit),
                   nullptr) == RS_SUCCESS) {
