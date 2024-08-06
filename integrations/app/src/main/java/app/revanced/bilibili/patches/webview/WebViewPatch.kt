f2c36a0f8af5d088666b27c1044b4ee25c52946d
Kofua <1638183271zjn@gmail.com>
2024-08-06 11:54:57 +0800

    fix: can't save webview notes sometimes
    DOM may not have been fully updated when `onPageFinished`.

--- integrations/app/src/main/java/app/revanced/bilibili/patches/webview/WebViewPatch.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/webview/WebViewPatch.kt
@@ -2,6 +2,7 @@ package app.revanced.bilibili.patches.webview
 
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.webkit.WebView.VisualStateCallback
 import android.webkit.WebViewClient
 import androidx.annotation.Keep
 import app.revanced.bilibili.settings.Settings
@@ -25,10 +26,14 @@ object WebViewPatch {
             override fun onPageFinished(view: WebView, url: String) {
                 if (!Settings.SaveCommentImage() && !Versions.ge7_76_0()) return
                 if (url.startsWith("https://www.bilibili.com/h5/note-app/view")) {
-                    view.evaluateJavascript(
-                        """(function(){for(var i=0;i<document.images.length;++i){var image=document.images[i];if(image.className==='img-preview'||image.parentElement.className==='img-preview'){image.addEventListener("contextmenu",(e)=>{hooker.saveImage(e.target.currentSrc)})}}})()""",
-                        null
-                    )
+                    view.postVisualStateCallback(0, object : VisualStateCallback() {
+                        override fun onComplete(requestId: Long) {
+                            view.evaluateJavascript(
+                                """(function(){for(var i=0;i<document.images.length;++i){var image=document.images[i];if(image.className==='img-preview'||image.parentElement.className==='img-preview'){image.addEventListener("contextmenu",(e)=>{hooker.saveImage(e.target.currentSrc)})}}})()""",
+                                null
+                            )
+                        }
+                    })
                 }
             }
         })
