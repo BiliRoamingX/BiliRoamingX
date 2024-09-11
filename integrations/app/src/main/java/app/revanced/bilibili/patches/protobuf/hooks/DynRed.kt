dc8233be90e34da31b0a31f77aaa5d19502abff7
Kofua <1638183271zjn@gmail.com>
2024-09-11 18:47:25 +0800

    fix: dynamic no up recall

--- integrations/app/src/main/java/app/revanced/bilibili/patches/protobuf/hooks/DynRed.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/protobuf/hooks/DynRed.kt
@@ -4,6 +4,7 @@ import app.revanced.bilibili.patches.protobuf.MossHook
 import app.revanced.bilibili.settings.Settings
 import app.revanced.bilibili.utils.runCatchingOrNull
 import app.revanced.bilibili.utils.toJSONObject
+import com.bapis.bilibili.app.dynamic.v1.CornerType
 import com.bapis.bilibili.app.dynamic.v1.DynRedReply
 import com.bapis.bilibili.app.dynamic.v1.DynRedReq
 import com.bilibili.lib.moss.api.MossException
@@ -26,12 +27,17 @@ object DynRed : MossHook<DynRedReq, DynRedReply>() {
                 clearBgType()
                 clearDisplayTime()
                 clearUp()
+                runCatchingOrNull {
+                    if (cornerInfo.cornerType == CornerType.corner_type_animation)
+                        clearCornerInfo()
+                }
             }
             runCatchingOrNull {
                 reply.tabRecallExtra.runCatchingOrNull { toJSONObject() }?.run {
                     put("tab_track_id", "")
                     put("tab_bg_type", 0)
                     put("display_time", 0)
+                    put("corner_type", 0)
                     put("tab_face_oid", 0)
                     put("tab_face_oid_type", 0)
                     reply.tabRecallExtra = toString()
