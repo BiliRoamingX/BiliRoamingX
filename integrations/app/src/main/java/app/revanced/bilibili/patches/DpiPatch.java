2075365eb4c11ee7a0044c3004c7b3f96752bca2
Kofua <1638183271zjn@gmail.com>
2024-09-18 23:54:56 +0800

    fix: danmaku mask
    Close #480

--- integrations/app/src/main/java/app/revanced/bilibili/patches/DpiPatch.java
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/DpiPatch.java
@@ -1,15 +1,10 @@
 package app.revanced.bilibili.patches;
 
-import android.app.Activity;
 import android.content.res.Resources;
 
 import androidx.annotation.Keep;
 
-import com.bilibili.bililive.room.ui.roomv3.LiveRoomActivityV3;
-
-import app.revanced.bilibili.patches.main.ApplicationDelegate;
 import app.revanced.bilibili.settings.Settings;
-import app.revanced.bilibili.utils.Versions;
 
 public class DpiPatch {
     public static final float displayScale = Float.parseFloat(Settings.DisplaySize.get());
@@ -19,14 +14,9 @@ public class DpiPatch {
         var scale = displayScale;
         if (scale == 0f)
             return density;
-        Activity topActivity = ApplicationDelegate.getTopActivity();
-        if (Versions.ge7_70_0() && topActivity instanceof LiveRoomActivityV3) {
-            var sysDensity = Resources.getSystem().getDisplayMetrics().density;
-            // for pink client 7.70.0+, live room player need right density to show content.
-            return sysDensity + scale;
-        } else {
-            // return system default density for making subtitle and danmaku size display normally.
-            return Resources.getSystem().getDisplayMetrics().density;
-        }
+        if (density == 1.0f)
+            return density;
+        var sysDensity = Resources.getSystem().getDisplayMetrics().density;
+        return sysDensity + scale;
     }
 }
